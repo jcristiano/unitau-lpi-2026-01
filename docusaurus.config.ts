c54cea9f7da4824b8ece930ff87bbc32b3bffcc8
Cristiano <jcristaino.c@gmail.com>
2026-03-15 14:04:40 -0300

    fix: corrigido link para edicao da documentacao

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -45,7 +45,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/jcristiano/unitau-lpi-2026-01/tree/main/',
         },
         blog: false,
         theme: {
